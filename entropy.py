c1bd6c572d1d9f22b75f808f066b57aa162b5764
Simon Burkhardt <simon@simonmartin.ch>
2022-01-22 16:07:19 +0100

    conditional entropy working (inefficiently!)

--- entropy.py
+++ entropy.py
@@ -14,7 +14,7 @@ import sys
 def entropy_estimation(filename, k):
     cnt = 0       # total byte length
     H = 0         # entropy
-    alphabet = {} # alphabet for symbols that will occurr
+    alphabet = {} # alphabet for symbols that will occur
     with open(filename, 'rb') as f:
         byte = [f.read(1) for i in range(k+1)] # array of one or multiple bytes
         while byte[-1] != b"":
@@ -34,24 +34,63 @@ def entropy_estimation(filename, k):
     return H
 
 def conditional_entropy_estimation(filename, k, l):
-    cnt = 0
-    alphabet = {}
+    cnt = 0       # total byte length
+    H = 0         # entropy
+    
+    # 3 different things must be counted in separate alphabets
+    # - single symbols (x_n+1)
+    # - previous k symbols (x_n, ... x_n-k)
+    # - previous k+1 symbols (xn+1, x_n, ... x_n-k)
+    
+    alphabet = {} # alphabet for previous k+1 symbols (xn+1, x_n, ... x_n-k)
+    alpha_single = {} # alphabet single symbols (x_n+1)
+    alpha_minus = {}  # alphabet for previous k symbols (x_n, ... x_n-k)
+
+    # count single characters and full sequence
     with open(filename, 'rb') as f:
         byte = [f.read(1) for i in range(k+1)]
         while byte[-1] != b"":
             key = "".join(str(_) for _ in byte)
             alphabet[key] = alphabet.get(key, 0) + 1
             cnt += 1
-            byte.pop(0)
+            b = byte.pop(0)
+            alpha_single[b] = alpha_single.get(b, 0) + 1
             byte.append(f.read(1))
-            
-    H = 0
-    
-    for key in alphabet:
-        p = alphabet[key]
-        if(p > 0):
-            p /= cnt
-            H += p * math.log2(p)
+         
+    k -=1
+    if (k<0):
+        k=0
+    cnt = 0
+    # run again but cound one less symbols
+    with open(filename, 'rb') as f:
+        byte = [f.read(1) for i in range(k+1)]
+        while byte[-1] != b"":
+            key = "".join(str(_) for _ in byte)
+            alpha_minus[key] = alpha_minus.get(key, 0) + 1
+            cnt += 1
+            b = byte.pop(0)
+            byte.append(f.read(1))   
+
+    # for each predecessing combination of k symbols: yi = { x_n, x_n-1 ...}
+    for tupl in alpha_minus: # for j=1 ... M
+        # calculate pY(yi)
+        py = alpha_minus[tupl] / sum(alpha_minus.values())
+        #print(f'py ( {tupl} ) = {py}')
+        ocr = 0
+        # calculate H (X|Y=yi)
+        for pair in alphabet:
+            if pair.startswith(str(tupl)):
+                ocr += alphabet[pair]
+        # conditional probability, given condition: startswith()
+        Hc = 0
+        for sym in alpha_single:  # for i=1 ... L
+            key = str(tupl) + str(sym)
+            #key = str(sym) + str(tupl)
+            if(key in alphabet): # not every combination may have occurred and is in the alphabet
+                pcond = alphabet[key] / ocr
+                # print(f'p(X|{key}) = {alphabet[key]} / {ocr}')
+                Hc += pcond * math.log2(pcond)
+        H += py * Hc
     H = -H
     return H
     
@@ -67,6 +106,7 @@ def file_length(filename):
     
 def main():
     filename = './infiles/cantrbry/xargs.1' # default for debuging
+    # filename = './infiles/cantrbry/ptt5' # default for debuging
     if(len(sys.argv) > 1):
         filename = sys.argv[1] # use first command line argument as file
     
