2ef3bc8a66b875b73e5648cb276bc3aedf7748b6
wilto837 <wilto837@student.liu.se>
2022-03-03 17:15:43 +0100

    LZ78 now works

--- LZ78.py
+++ LZ78.py
@@ -1,6 +1,10 @@
+import time
 
-filename = ["cantrbry/test.txt"]
+filenames = ["cantrbry/xargs.1", "cantrbry/fields.c"]
 
+shortfilenames = ["cantrbry/alice29.txt", "cantrbry/asyoulik.txt", "cantrbry/cp.html", "cantrbry/fields.c", "cantrbry/grammar.lsp", "cantrbry/kennedy.xls", "cantrbry/lcet10.txt",
+                  "cantrbry/plrabn12.txt", "cantrbry/ptt5", "cantrbry/sum", "cantrbry/xargs.1"]
+bigfilenames = ["large/bible.txt", "large/E.coli", "large/world192.txt"]
 
 def read_file(filename):
     # Load bytes from file and count symbol occurances
@@ -8,109 +12,110 @@ def read_file(filename):
         data = f.read()
         return data
 
-
-data = read_file(filename[0])
-
-datasize = len(data)
-
-list = [(0, None)]
-index = 0   # index for data
-nextsymbol = False
-symbol_match_index = 0 #empty symbol
-run = True
-
-match_length = 1 # symbol chain from data
-data_to_compare_with = 0 # how many bytes in data to compare with list
-
-
-print("Coding: ", filename, " with ", datasize, " bytes!\n")
-
-# Example:
-# a b a d a b a c b a c b a c
-# (0, None), (0,a), (0,b), (1,d), (1,b), (1,c), (2,a), (0,c), (6,c)
-
-# It is easy when implementing to code one symbol at a time
-# harder when you need to compare multiple symbols in data with multiple symbols in list
-# because every element in the list only contains one symbol and a reference to another one.
-# That is what I try to do down here
-
-while run:
-    if(index +1 > datasize): # done?
-        # code the last symbol
-        list.append((symbol_match_index, data[index-1]))
-        print("added new symbol: ", (symbol_match_index, data[index-1]), "\n")
-        break
-    print("Coding byte ", index, " , with data: ", data[index], "\n")
-
-
-    ### Actual coding and looking for symbols in list starts here
-
-    data_byte = data[index] # current byte from data to code
-
-    longest_match = 0 # variable to remember the longest match
-    longest_match_index = 0 # variable to remember the index of longest match
-
-    # look if data_byta already exists in list
-    for i in range(len(list)):
-
-        # list_byte must be the deepest down in the list when comparing
-        # Ex: (0,a),(0,b),(1,a) which is abba
-        # but when reading list[2][1] is will give "a" but list[2][1] is actually ba
-        list_byte = None
-        j = i
-        while(list[j][1] != None):
-            if list[list[j][0]][1] == None: # get byte before None byte
-                break
-            j = list[j][0]
-
-        
-        list_byte = list[j][1] # first byte of byte chain in list
-
-        if(list_byte == data_byte):
-            ### Match!!!
-
-            # Need to look at the deepest byte ex: (1,b) need to look at byte at 1 before b and so on until None is the byte
-
-            # add first match to start of byte_chain
-            byte_chain = []
-            byte_chain.insert(0,(i,data_byte))
-
-            # gets the whole byte chain from the element in list
-            k = list[i][0]
-            deeper_byte = list[k][1]
-
-            current_longest_match_index = 0 # reference to (0, None)
-
-            while deeper_byte != None:
-                byte_chain.insert(0,(k,deeper_byte)) # need to store index for later use
-                k = list[k][0]
-                deeper_byte = list[k][1]
-            
-            # byte_chain is now a list with the whole byte chain stored in list ex: [a,b,b,a,a]
-            print("Byte chain: ", byte_chain)
-
-            # Now we need to calculate how long the match is for this element in the list with the data
-            currentmatch = 0
-            for i in range(len(byte_chain)):
-                if byte_chain[i][1] == data[index+i]:
-                    currentmatch += 1
-                    longest_match_index = byte_chain[i][0]
-
-            if currentmatch > longest_match:
-                longest_match = currentmatch
-            
-            print("Byte chain from list: ", byte_chain)
-
-        # no match => compare with next symbol in list
+def get_byte_sequence_from_element_in_list(i):
+    # returns a list of the sequence that an index in list refers to
+    byte_chain = []
+    while list[list[i][0]][1] != None:
+        byte_chain.insert(0,(i,list[i][1]))
+        i = list[i][0]
     
-    # More than one symbol already exists in list
-    if longest_match > 0:
-        list.append((longest_match_index, data[index + longest_match]))
-        index += 1 + longest_match
-    else:
-        # Symbol does not exist in list => add new symbol
-        list.append((0, data[index]))
-        match_length = 1 # restore match_length
-        index += 1 # code next byte in data
+    byte_chain.insert(0,(i,list[i][1]))
+    
+    #print("byte_chain: ", byte_chain)
+    return byte_chain
 
-print("Code:\n", list)
+
+
+def get_first_symbol_from_element_in_list(i):
+    # returns the first symbol that an element in list refers to
+    while(list[i][1] != None):
+        if list[list[i][0]][1] == None: # get byte before None byte
+            return list[i][1] # first byte of byte chain in list
+        i = list[i][0]
+    
+    return list[i][1]
+
+
+def LZ78_file(filename, list):
+    # Example:
+    # a b a d a b a c b a c b a c
+    # (0, None), (0,a), (0,b), (1,d), (1,b), (1,c), (2,a), (0,c), (6,c)
+
+    # It is easy when implementing to code one symbol at a time
+    # harder when you need to compare multiple symbols in data with multiple symbols in list
+    # because every element in the list only contains one symbol and a reference to another one.
+    # That is what I try to do down here
+
+    data = read_file(filename)
+    datasize = len(data)
+    index = 0   # indexing for bytes in data
+    print("Coding: ", filename, " with ", datasize, " bytes!\n")
+
+    while True:
+
+        if index > datasize-1:
+            break
+
+        data_byte = data[index] # current byte from data to code
+
+        longest_match = 0 # variable to remember the longest match
+        longest_match_index = 0 # variable to remember the index of longest match
+
+        # look if data_byta already exists in list and find the longest match
+        for i in range(len(list)):
+
+            # list_byte must be the deepest down in the list when comparing
+            # Ex: (0,a),(0,b),(1,a) which is abba
+            # but when reading list[2][1] is will give "a" but list[2][1] is actually ba
+            
+            list_byte = get_first_symbol_from_element_in_list(i)
+
+            if(list_byte == data_byte):
+                ### Match!!!
+
+                # Need to look at the deepest byte ex: (1,b) need to look at byte at 1 before b and so on until None is the byte
+
+                # add first match to start of byte_chain
+                byte_chain = get_byte_sequence_from_element_in_list(i)
+
+                # Calculate how many bytes in data that match with list
+                currentmatch_length = 0
+                currentmatch_index = 0
+
+                for j in range(len(byte_chain)):
+                    if index + j < datasize - 1:
+                        if byte_chain[j][1] == data[index+j]:
+                            currentmatch_length += 1
+                            currentmatch_index = byte_chain[j][0]
+                    else:
+                        break # do not code more bytes than in data
+                
+                #print("matchlength: ", currentmatch_length, " index: ", currentmatch_index)
+
+                if currentmatch_length > longest_match:
+                    longest_match = currentmatch_length
+                    longest_match_index = currentmatch_index
+                
+            # no match => compare with next symbol in list
+        
+        # More than one symbol already exists in list
+        if longest_match > 0:
+            list.append((longest_match_index, data[index + longest_match]))
+            index += longest_match + 1
+            #print("list: ", list)
+        else:
+            # Symbol does not exist in list => add new symbol
+            list.append((0, data[index]))
+            index += 1 # code next byte in data
+
+    return list
+
+
+
+for filename in filenames:
+    list = [(0, None)]
+    start = time.time()
+    code = LZ78_file(filename, list)
+    end = time.time()
+    print("Coding ", filename, " took ", end-start, " seconds!\n")
+    #print(code)
