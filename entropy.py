f6e4e4574032f9987b528bacbceca3c63eca10e5
Justus Rossmeier <veecue@ventos.tk>
2022-03-14 17:28:57 +0100

    entropy: Generate nice plot

--- entropy.py
+++ entropy.py
@@ -1,6 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from glob import glob
+import pandas as pd
+import os
 
 
 def file_to_array(path):
@@ -28,13 +30,46 @@ def entropy_combined(x, num, size=256):
     return entropy(out, size**num)
 
 
-paths = glob("cantrbry/*")
+paths = glob("cantrbry/*") + glob("large/*")
 # path = paths[0]
 # path = "large/world192.txt"
+
+filenames = []
+H0s = []
+H1s = []
+H2s = []
 
 for path in paths:
     data = file_to_array(path)
     H1 = entropy_combined(data, 1)
     H2 = entropy_combined(data, 2)
     H3 = entropy_combined(data, 3)
-    print(path, H1, H2, H3, sep="\t")
+    filenames.append(os.path.basename(path))
+    H0s.append(H1)
+    H1s.append(H2 - H1)
+    H2s.append(H3 - H2)
+
+    print(
+        *([path] + list(map("{:.2f}".format, [H1, H2, H3, H2 - H1, H3 - H2]))), sep="&"
+    )
+
+H0s = np.array(H0s)
+H1s = np.array(H1s)
+H2s = np.array(H2s)
+
+# normalized visualization
+# H2s /= H0s
+# H1s /= H0s
+# H0s /= H0s
+plt.subplot(1, 1, 1)
+plt.subplots_adjust(bottom=0.25)
+
+bot = plt.bar(filenames, H2s, label="$H(X_i|X_{i-1},X_{i-2})$")
+bot = plt.bar(filenames, H1s - H2s, bottom=H2s, label="$H(X_i|X_{i-1})$")
+bot = plt.bar(filenames, H0s - H1s, bottom=H1s, label="$H(X_i)$")
+
+plt.tick_params(labelrotation=90)
+plt.ylabel("Entropy in bits/symbol")
+plt.title("Comparison of Memory")
+plt.legend()
+plt.show()
